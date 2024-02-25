a8c7c9f3e779911356099b40fbdea68451889042
Diana <lici4qweru090@gmail.com>
2024-02-25 19:49:49 +0500

    added Bootstrap

--- demo/src/main/java/com/example/demo/model/Post.java
+++ demo/src/main/java/com/example/demo/model/Post.java
@@ -1,10 +1,15 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
-    private Integer likes;
-    public Post(String text) {
+    private Integer likes = 0;
+    private Date creationDate;
+
+    public Post(String text, Date creationDate) {
         this.text = text;
+        this.creationDate = creationDate;
     }
 
     public String getText() {
@@ -12,5 +17,8 @@ public class Post {
     }
     public Integer getLikes() {
         return likes;
+    }
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
--- demo/src/main/java/com/example/demo/service/PostService.java
+++ demo/src/main/java/com/example/demo/service/PostService.java
@@ -3,15 +3,16 @@ import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts() {
         List<Post> posts = new ArrayList<>();
-        posts.add(new Post("пост раз"));
-        posts.add(new Post("пост два"));
-        posts.add(new Post("пост три"));
+        posts.add(new Post("пост раз", new Date()));
+        posts.add(new Post("пост два", new Date()));
+        posts.add(new Post("пост три", new Date()));
         return posts;
     }
 }
