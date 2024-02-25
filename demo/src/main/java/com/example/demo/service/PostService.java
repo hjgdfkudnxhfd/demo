99630d1a407acf1b2e8e808908ee19b4a548f1ef
Diana <lici4qweru090@gmail.com>
2024-02-25 20:58:07 +0500

    отображаются новые посты

--- demo/src/main/java/com/example/demo/service/PostService.java
+++ demo/src/main/java/com/example/demo/service/PostService.java
@@ -1,4 +1,5 @@
 package com.example.demo.service;
+
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
@@ -8,11 +9,16 @@ import java.util.List;
 
 @Service
 public class PostService {
-    public List<Post> listAllPosts() {
-        List<Post> posts = new ArrayList<>();
-        posts.add(new Post("пост раз", new Date()));
-        posts.add(new Post("пост два", new Date()));
-        posts.add(new Post("пост три", new Date()));
+    List<Post> posts = new ArrayList<>();
+    public List<Post> listAllPosts(){
+        //posts = new ArrayList<>();
+        posts.add(new Post("Пост 1", new Date()));
+        posts.add(new Post("Пост 2", new Date()));
+        posts.add(new Post("Пост 3", new Date()));
         return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
